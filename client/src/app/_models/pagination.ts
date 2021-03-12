8820f2118e1825ec27cc9b675f956e58159fb30d
Scitrix <szeszak.adam@gmail.com>
2021-03-12 08:45:33 +0100

    End of section 13

---
+++ client/src/app/_models/pagination.ts
@@ -0,0 +1,11 @@
+export interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalpages: number;
+}
+
+export class PaginatiedResult<T> {
+  result: T;
+  pagination: Pagination;
+}
